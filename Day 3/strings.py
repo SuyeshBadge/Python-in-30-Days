8607b47d201e5ce08661b11978d3cdaaa20bcd6c
Suyesh Badge <suyeshbadge@gmail.com>
2021-05-15 18:30:01 +0530

    Day 3

---
+++ Day 3/strings.py
@@ -0,0 +1,91 @@
+# String
+'''
+ex:
+a = 'abcd'
+b = 'b'
+n=input()
+print(' string ')
+print(232 + '233') wrong
+'''
+
+# a = '1 2 3 4 5 6 7 8 9'
+# l = a.split(' ')
+# print(a)
+
+# print(':'.join(l))
+
+# a = 'HIMANSHU'
+# print(len(a))
+# print(a.capitalize())
+# print(a.count('h'))
+# # print(a.index('h'))
+# print(a.lower())
+'''
+
+
+
+q1:
+wap to take values from user and print the in a represented manner
+ex
+
+inp:
+ name ,age ,gender
+op:
+
+Hello {name} your age is {age} and your gender is {gender}
+
+
+
+################################
+
+
+
+
+q2:
+
+ Write a programm to count the number of vowels in the given string
+
+
+a="harshal"
+
+
+'''
+a = 'H a r s h a l'
+print(a[0])
+print(a[6])
+
+# print(a[len(a)])
+la = len(a)
+print(a[la-1])
+print(a[-1])
+print(a)
+# b=a.split()
+# print(b[::-1])
+print(a[::-3])
+print(a[1:])
+
+# print(a[-1:0:])
+# string[start:end:step]
+
+'''
+
+write a program to reverse the string
+
+wap to replace the characters of the given index from the user in a given string
+
+ip
+enter the str:'hello'
+enter the character :'a'
+enter the index :2
+
+
+a=hello
+a[2]='a'
+
+op
+
+The new string is :healo
+
+
+
+'''
