1b60716bd3b49eca9a20a6d3acbea32faee02437
Suyesh Badge <suyeshbadge@gmail.com>
2021-05-15 18:30:14 +0530

    Day 3

---
+++ Day 3/numbers.py
@@ -0,0 +1,91 @@
+import math
+from math import ceil, log2
+"""
+floating numbers
+complex numbers
+math library
+
+
+"""
+# floating Numbers
+'''
+
+a = 5
+b = 1.4000
+# print(type(a))
+# c = a+b
+# print(type(c))
+
+
+print(type(b))
+print(b)
+print('{0:.6f}'.format(b))
+print(f'{b:.3f}')
+
+
+'''
+# complex Numbers
+'''
+ex
+c=3+3j
+c = 3 + 3j
+print(c)
+print(type(c))
+'''
+# math library
+
+
+# math.ceil
+# math.floor
+# math.log10
+# math.log2
+
+# # abs()  # absolute value
+# 5-3 = 2
+# 3-5 = -2
+
+# a = int(input())
+# b = float(input())
+
+# print(abs(a-b))
+# print(abs(3-5))10
+
+# sqrt()
+# print(math.sqrt(3))
+
+# ceil()
+'''
+
+7
+7.88888 ~ 8
+6.944
+print(math.ceil(7.00088))
+print(math.floor(7.999))
+'''
+'''
+log10 => base 10
+
+log(10) => 10^1 = 1
+log(100) => 10^2 = 2
+log(1000) => 10^3 = 3
+
+10 x 1 = 10 => 1
+10 x 10 = 100 => 2
+10 x 10 x 10 => 3
+
+
+
+log2 => base 2
+
+log(2) => 1
+log(4) => 2
+log(8) => 3
+log(16) => 4
+
+log3 =>3
+log(3) =1
+log(2) = ?
+
+print(math.log10(100))
+
+'''
